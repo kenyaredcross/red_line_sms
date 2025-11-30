9d3af660e8f929f4c33f3e02e2966571fc279cfc
NjengaSheba <njengasheba@gmail.com>
2025-11-30 18:16:17 +0300

    removed character_count

--- red_line_sms/api/v1/sms_data.py
+++ red_line_sms/api/v1/sms_data.py
@@ -25,7 +25,7 @@ def get_sms_data(limit = 100):
             "Send SMS", 
             fields = ["name", "created_by", "created_on", "to_be_sent_on", "workflow_state", "status",
                 "receiver_type", "phone_numbers", "message",
-                "character_count", "response_log", "total_sent_sms",
+                 "response_log", "total_sent_sms",
                 "total_failed", "total_cost",
             ],
             limit = limit, 
